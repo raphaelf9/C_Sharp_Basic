b640b6bfd1db61e3dc15a7312da357ca1f8c71f8
Raphael Faria de Souza <68441857+raphaelf9@users.noreply.github.com>
2022-06-22 09:59:00 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números inteiros você vai digitar? ");
+            int n1 = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for(int i = 1; i <= n1; i++)
+            {
+                Console.WriteLine("Valor #" + i);
+                int n2 = int.Parse(Console.ReadLine());
+                soma += n2;
+            }
+
+            Console.WriteLine("Total da soma = " + soma);
+        }
+    }
+}
