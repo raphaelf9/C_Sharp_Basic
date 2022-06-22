8cab866a944f1493fb85fca66234b41b2e4a9723
Raphael Faria de Souza <68441857+raphaelf9@users.noreply.github.com>
2022-06-22 10:11:12 -0300

    Add files via upload

---
+++ Sobrecarga/Sobrecarga/Produto.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Sobrecarga
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Construtor padrão declarado para permitir sua inicialização no programa principal
+        public Produto()
+        {
+
+        }
+
+        //Construtor com valores inicializados
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        /*//Construtor usando 'this' para pegar a quantidade vindo do construtor principal
+        public Produto(string nome, double preco) : this ()
+        {
+            Nome = nome;
+            Preco = preco;
+        }*/
+
+        //Construtor de sobrecarga com parâmetros diferentes
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+
