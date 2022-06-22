c9c9584043487241d30869f81f49c61a22d2e3f5
Raphael Faria de Souza <68441857+raphaelf9@users.noreply.github.com>
2022-06-22 10:08:55 -0300

    Add files via upload

---
+++ Inferencia_de_tipo/Inferencia_de_tipo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inferencia_de_tipo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 32; //tipo a ser inferido pela linguagem
+            var y = 2.00;
+            var z = "Maria";
+
+
+
+        }
+    }
+}
