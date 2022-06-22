f8efa03e9498340e614f2ab5b5e75d1a1215b93f
Raphael Faria de Souza <68441857+raphaelf9@users.noreply.github.com>
2022-06-22 10:10:23 -0300

    Add files via upload

---
+++ Params/Params/Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Params
+{
+    class Calculadora
+    {
+
+        public static int Soma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int Soma2(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        public static int Soma3(int n1, int n2, int n3, int n4)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+
+        public static int Soma4(params int[] numeros)
+        {
+            int soma = 0;
+            for(int i = 0; i< numeros.Length; i++)
+            {
+                soma += numeros[i];
+            }
+
+            return soma;
+        }
+    }
+}
---
+++ Params/Params/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculadora.Soma(2, 3);
+            int s2 = Calculadora.Soma4(20, 30, 40, 50, 60, 70);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+
+        }
+    }
+}
